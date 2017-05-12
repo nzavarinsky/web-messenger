b0fabba89a01adb7a2faf8b61050b4ba058fde60
nzavarinsky <nazarko1805>
2017-05-12 16:00:23 +0300

    [+]

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
  
-app.get('/Client/', function(req, res){
+app.get('/', function(req, res){
   res.sendfile('index.html');
 });
  
